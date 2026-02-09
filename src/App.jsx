4c0380cde5764c8b2cda06705288e0ecc98380e7
Mahtab Khan <khan.mahtab0603@gmail.com>
2026-02-09 15:49:04 +0530

    Created All Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct } from "./pages";
 const App = () => {
 
   return(
-    <h1 className="text-5xl font-bold">Hello World</h1>
+    <div>
+      <h1 className="text-5xl font-bold">Hello World</h1>
+      <button
+  className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
+<About />
+<Cart />
+    </div>
   )
 }
 export default App;
